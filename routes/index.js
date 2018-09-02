1f7602cbc0d97b82ef9a58d10d866fe490746a6f
Sam Billingham <sambillie.sb@gmail.com>
2018-09-02 16:34:52 +0200

    add subdomain routing checks/flow

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,43 @@ var rp = require('request-promise');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //  res.render('profile-example', {
-  //     title: 'Utopian Dashy'
-  //  });
-   res.render('index');
+  const domain = req.headers.host;
+  const subDomain = domain.split('.');
+
+  if(subDomain.length > 2) {
+      username = subDomain[0]
+      res.render('profile', {username} );
+  } else {
+      res.render('index');
+  }
 });
 
 router.get('/@:username', (req, res) => {
   const username = req.params.username
-  res.render('profile', {username} );
+
+  let domain = req.headers.host;
+  let subDomain = domain.split('.');
+
+  if(subDomain.length > 2) {
+      res.redirect('/');
+  } else {
+      res.render('profile', {username} );
+  }
 });
 
+router.get('/:permlink', (req, res) => {
+  const domain = req.headers.host;
+  const subDomain = domain.split('.');
+  const username = subDomain[0];
+  const permlink = req.params.permlink
+
+  if(subDomain.length > 2) {
+    res.render('single', {username, permlink} );
+  } else {
+    let err = new Error('Not Found');
+    err.status = 404;
+  }
+});
 
 router.get('/@:username/:permlink', (req, res) => {
   const username = req.params.username
