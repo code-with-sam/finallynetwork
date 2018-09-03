e39289c96cc59496e81cac775cd50ae0f9071195
Sam Billingham <sambillie.sb@gmail.com>
2018-09-03 09:34:57 +0200

    rewrite hckr-theme js

--- public/js/hckr-theme.js
+++ public/js/hckr-theme.js
@@ -1,48 +1,87 @@
-let page;
+const hckr = {
+  permlink: $('main').data('permlink'),
+  username: $('main').data('username'),
+  lastPermlink: '',
 
-if($('main').hasClass('profile') ) {
-  loadUserPosts(false)
-} else {
-  loadSinglePost()
-}
+  init(){
+    $('main').addClass('hckr-theme')
+    hckr.uiActions()
+    hckr.isBlogFeed() ? hckr.initBlogFeed(false) : hckr.loadSinglePost()
+  },
 
-function loadUserPosts(loadMore) {
-  const username = $('main').data('username')
-  let query = { tag: username, limit: 10 }
-  const listPosts = (posts) => {
-    if (posts.length < 10) $('.load-more-posts').remove()
-    for (var i = 0; i < posts.length; i++) {
-      if(loadMore && i === 0) continue
-      let template = `<div>
-        <h2><a href="@${username}/${posts[i].permlink}" target="_blank"> ${posts[i].title}</a></h2>
-        <h3>${moment(posts[i].created).format("DD/MM/YY")  } | comments: ${posts[i].children} | votes: ${posts[i].net_votes}</h3>
-      </div>`
-      $('main').append(template)
+  uiActions() {
+    $('main').on('click','.load-more-posts', (e) => {
+      e.preventDefault()
+      hckr.loadUserPosts(true)
+    })
+  },
+
+  isBlogFeed(){
+    return $('main').hasClass('profile')
+  },
+
+  async loadSinglePost(){
+    const postData = await steem.api.getContentAsync(hckr.username, hckr.permlink)
+    hckr.appendSingePostContent(postData)
+    // finallycomments.init()
+    // finallycomments.loadEmbed('.single-post__finally-comments')
+  },
+
+  appendSingePostContent(post) {
+    var converter = new showdown.Converter();
+    // var html = purify.sanitize(converter.makeHtml(post.body))
+    var html = converter.makeHtml(post.body)
+    let template = `<h2>${post.title}</h2>${html}`
+    $('main').append(template)
+  },
+
+  initBlogFeed(){
+    $('main').append(hckr.blogFeedTemplate())
+    hckr.loadUserPosts(false)
+
+  },
+
+  loadUserPosts(loadMore) {
+    let query = { tag: hckr.username, limit: 10 }
+    if(loadMore) {
+    query = { tag: hckr.username, limit: 10, start_author: hckr.username,
+      start_permlink: hckr.lastPermlink }
     }
+    steem.api.getDiscussionsByBlog(query, (err, result) => {
+      console.log(result)
+      if (err === null) hckr.loopUserPosts(loadMore, result)
+    })
+  },
+
+  loopUserPosts(loadMore, posts){
+      hckr.lastPermlink = posts[posts.length -1].permlink
+      if (posts.length < 10) $('.load-more-posts').remove()
+      for (var i = 0; i < posts.length; i++) {
+        if(loadMore && i === 0) continue
+        hckr.appendPostItem(posts[i])
+      }
+  },
+
+  appendPostItem(post){
+    let template = hckr.blogFeedItemTemplate(post)
+    $('.blog-feed').append(template)
+  },
+
+  blogFeedTemplate(){
+      return `
+      <header><h1>${hckr.username}</h1></header>
+      <section class="blog-feed"></section>
+      <section><a class="load-more-posts" href="#">Load More Posts</a></section>
+      `
+  },
+
+  blogFeedItemTemplate(post){
+    return `<div>
+      <h2><a href="@${hckr.username}/${post.permlink}"> ${post.title}</a></h2>
+      <h3>${moment(post.created).format("DD/MM/YY")  } | comments: ${post.children} | votes: ${post.net_votes}</h3>
+    </div>`
   }
-  if(loadMore) {
-  query = { tag: username, limit: 10, start_author: username,
-    start_permlink: $('tr').last().data('permlink') }
-  }
-  steem.api.getDiscussionsByBlog(query, (err, result) => {
-    console.log(result)
-    if (err === null) listPosts(result)
-  })
+
 }
 
-async function loadSinglePost(){
-  const permlink = $('main').data('permlink')
-  const username = $('main').data('username')
-  const postData = await steem.api.getContentAsync(username, permlink)
-  appendSingePostContent(postData)
-  // finallycomments.init()
-  // finallycomments.loadEmbed('.single-post__finally-comments')
-}
-
-function appendSingePostContent(post) {
-  var converter = new showdown.Converter();
-  // var html = purify.sanitize(converter.makeHtml(post.body))
-  var html = converter.makeHtml(post.body)
-  let template = `<h2>${post.title}</h2>${html}`
-  $('main').append(template)
-}
+hckr.init()
