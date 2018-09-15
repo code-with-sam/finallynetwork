454842d56e1b01294686821ee94bf70f89a8bffe
Sam Billingham <sambillie.sb@gmail.com>
2018-09-15 15:58:47 +0200

    update landing page for beta

--- platform/src/js/main.js
+++ platform/src/js/main.js
@@ -1,13 +1,20 @@
 import $ from 'jquery'
-console.log('hello')
 
-$('.header__beta').on('click', () => {
-  $('.modal').fadeIn()
-})
-$('.modal .submit').on('click', (e)=> {
-  let username = $('.modal .username').val()
+const app = {
+  init() {
+    this.UiActions()
+  },
 
-  window.location.href = `/@${username}`
-  console.log('hello')
-  e.preventDefault()
-})
+  UiActions() {
+    $('.header__beta').on('click', () => $('.modal').fadeIn() )
+    $('.modal .submit').on('click', (e) => this.showBetaModal(e) )
+  },
+
+  showBetaModal(e){
+      let username = $('.modal .username').val()
+      window.location.href = `/@${username}`
+      e.preventDefault()
+  }
+}
+
+app.init()
