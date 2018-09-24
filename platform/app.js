6a942e45e879b982d243bb8e864627202ec8e321
Sam Billingham <sambillie.sb@gmail.com>
2018-09-24 21:53:13 +0100

    add steemconnect authentication

--- platform/app.js
+++ platform/app.js
@@ -4,18 +4,26 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
+let session = require('express-session');
 
 let env = require('dotenv').config()
 
+var routes = require('./routes/index');
+var auth = require('./routes/auth');
+
+let config = require('./config')
+
 var app = express();
 
-// view engine setup
+app.use(session({
+    secret: config.session.secret,
+    saveUninitialized: true,
+    resave: false
+}));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -23,6 +31,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.use('/auth', auth);
+app.use('/logout', auth);
 app.use('/', routes);
 
 // catch 404 and forward to error handler
