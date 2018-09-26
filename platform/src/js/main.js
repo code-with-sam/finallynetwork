a73bdf46095f9e16c8d58a2054a17ef998d8303b
Sam Billingham <sambillie.sb@gmail.com>
2018-09-26 08:50:33 +0100

    fetch/display news links from blockchain

--- platform/src/js/main.js
+++ platform/src/js/main.js
@@ -1,9 +1,11 @@
 import $ from 'jquery'
+import steem from 'steem'
 
 const app = {
   init() {
     this.UiActions()
     this.showSelectedThemeInDropdown()
+    this.loadNewsLinks()
   },
 
   UiActions() {
@@ -34,7 +36,23 @@ const app = {
         data: { theme, tag }
       },
       (response) => location.reload())
+  },
+
+  loadNewsLinks(){
+    const query = { tag: 'finallynetwork', limit: 5 }
+    const listPosts = (posts) => {
+      for (var i = 0; i < posts.length; i++) {
+        let template = `<li><a href="/blog/${posts[i].url}"> ${posts[i].title}</a></li>`
+        $('.landing__news ul').append(template)
+      }
+    }
+    steem.api.getDiscussionsByBlog(query, (err, result) => {
+      console.log('ho')
+      if (err === null) listPosts(result)
+    })
+
   }
+
 }
 
 app.init()
