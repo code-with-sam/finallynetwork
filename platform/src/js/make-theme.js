b7cd8aae7ebb0f4b0b64368d3294d263fc802b48
Sam Billingham <sambillie.sb@gmail.com>
2018-10-09 13:25:05 +0200

    WIP - make theme feed templates

--- platform/src/js/make-theme.js
+++ platform/src/js/make-theme.js
@@ -17,16 +17,45 @@ const make = {
 
   blogFeedTemplate(){
       return `
-      <header><h1>${make.username}</h1></header>
+      <header class="header">
+        <section class="header__social">
+          <span class="header__social-icon"><a href=""><img src="http://placehold.it/40x40"></a></span>
+          <span class="header__social-icon"><a href=""><img src="http://placehold.it/40x40"></a></span>
+          <span class="header__social-icon"><a href=""><img src="http://placehold.it/40x40"></a></span>
+          <span class="header__social-icon"><a href=""><img src="http://placehold.it/40x40"></a></span>
+        </section>
+        <section class="header__title">
+          <img class="header__avatar" src="http://placehold.it/150x150">
+        </section>
+        <section class="header__mail">
+          <span class="header__social-icon"><a href=""><img src="http://placehold.it/40x40"></a></span>
+        </section>
+      </header>
+      <section class="header__tagline"><h2>BECAUSE FOOD TASTES BETTER WITH MORE FOOD...</h2></section>
+      <nav class="nav">
+        <a class="nav__link">LINK ONE</a>
+        <a class="nav__link">LINK TWO</a>
+      </nav>
       <section class="blog-feed"></section>
-      <section><a class="load-more-posts" href="#">Load More Posts</a></section>
+      <section class="more-posts">
+        <a class="load-more-posts" href="#">Load More Posts</a>
+      </section>
       `
   },
 
-  blogFeedItemTemplate(post){
-    return `<div class="blog-feed__item">
-      <h2><a href="/@${make.username}/${post.permlink}"> ${post.title}</a></h2>
-      <h3>${moment(post.created).format("DD/MM/YY")  } | comments: ${post.children} | votes: ${post.net_votes}</h3>
+  blogFeedItemTemplate(post, featureImageSrc, tags, excerpt){
+    return `
+    <div class="blog-feed__item">
+      <a class="feed-item__feature" href="/@${make.username}/${post.permlink}">
+        <div class="feed-item__overlay">
+          <h3 class="feed-item__overlay-title">Read More</h3>
+        </div>
+        <img src="${featureImageSrc}">
+      </a>
+      <div class="feed-item__tags">${tags}</div>
+      <h2 class="feed-item__title"><a href="/@${make.username}/${post.permlink}"> ${post.title}</a></h2>
+      <div class="feed-item__excerpt">${excerpt}</div>
+      <h3>${post.net_votes}</h3>
     </div>`
   },
 
--- platform/src/js/modules/finally-core.js
+++ platform/src/js/modules/finally-core.js
@@ -5,7 +5,9 @@ import showdown from 'showdown'
 import finallycomments from 'finallycomments'
 import purify from 'dompurify'
 import moment from 'moment'
+import striptags from 'striptags'
 
+const POST_LIMIT = 15;
 
 let theme = {
   permlink: $('main').data('permlink'),
@@ -65,15 +67,16 @@ let theme = {
   },
 
   loadUserPosts(loadMore) {
-    let query = { tag: theme.username, limit: 15 }
+    let query = { tag: theme.username, limit: POST_LIMIT }
     if(loadMore) {
-    query = { tag: theme.username, limit: 10, start_author: theme.username,
+    query = { tag: theme.username, limit: POST_LIMIT, start_author: theme.username,
       start_permlink: theme.lastPermlink }
     }
     steem.api.getDiscussionsByBlog(query, (err, result) => {
-      result = theme.filterOutResteems(result, theme.username)
-      let posts = theme.tag !== '' ? theme.filterByTag(result, theme.tag) : result
-      if (err === null) theme.loopUserPosts(loadMore, posts)
+      console.log(result)
+      let resultLessResteems = theme.filterOutResteems(result, theme.username)
+      let posts = theme.tag !== '' ? theme.filterByTag(resultLessResteems, theme.tag) : resultLessResteems
+      if (err === null) theme.loopUserPosts(loadMore, posts, result.length)
     })
   },
 
@@ -88,9 +91,10 @@ let theme = {
     return posts.filter(post => post.author === username)
   },
 
-  loopUserPosts(loadMore, posts){
+  loopUserPosts(loadMore, posts, resultTotal){
+      if (theme.lastPermlink == posts[posts.length -1].permlink) $('.load-more-posts').remove()
       theme.lastPermlink = posts[posts.length -1].permlink
-      if (posts.length < 10) $('.load-more-posts').remove()
+      if (resultTotal < POST_LIMIT) $('.load-more-posts').remove()
       for (var i = 0; i < posts.length; i++) {
         if(loadMore && i === 0) continue
         theme.appendPostItem(posts[i])
@@ -98,9 +102,41 @@ let theme = {
   },
 
   appendPostItem(post){
-    let template = theme.blogFeedItemTemplate(post)
+    const tags = theme.getPostTags(post)
+    const excerpt = theme.getPostExcerpt(post)
+    const featureImageSrc = theme.generatePostFeatureImage(post)
+    const template = theme.blogFeedItemTemplate(post, featureImageSrc, tags, excerpt)
     $('.blog-feed').append(template)
+  },
+
+  getPostTags(post){
+    const tags = JSON.parse(post.json_metadata).tags
+    return tags.map( t => `<span class="tag">${t}</span>`).join(' ')
+  },
+
+  getPostExcerpt(post){
+    const converter = new showdown.Converter();
+    let placeholder = document.createElement('div');
+    placeholder.innerHTML = purify.sanitize(converter.makeHtml(post.body))
+    placeholder = placeholder.querySelector('p').innerHTML;
+    return striptags(placeholder)
+  },
+
+
+  generatePostFeatureImage(post){
+    let image
+    if( typeof JSON.parse(post.json_metadata).image === 'undefined' ){
+      const converter = new showdown.Converter();
+      const placeholder = document.createElement('div');
+      placeholder.innerHTML = purify.sanitize(converter.makeHtml(post.body))
+      let image = placeholder.querySelector('img');
+      return image ? image.src : '';
+    } else {
+      image = JSON.parse(post.json_metadata).image[0]
+    }
+    return image;
   }
+
 }
 
 module.exports.init = (name, blogFeedTemplate, blogFeedItemTemplate, singlePageTemplate) => {
