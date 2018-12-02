12a33592d6b6b277f2dc4d11026ff5f5f8392448
Sam Billingham <sambillie.sb@gmail.com>
2018-12-02 15:36:55 +0100

    add custom data entry for critday theme

--- platform/src/js/modules/finally-dashboard.js
+++ platform/src/js/modules/finally-dashboard.js
@@ -29,13 +29,28 @@ const dashboard = {
     const nav = $('.dashboard__nav-select').val();
     const showResteems = $('.dashboard__resteem-checkbox').is(':checked')
     const username = $('.dashboard').data('username')
+    const userData = Object.assign( { theme, tag, nav, showResteems }, this.setUserData())
+
     e.preventDefault()
     $.post({
       url: `/api/${username}/update`,
       dataType: 'json',
-      data: { theme, tag, nav, showResteems }
+      data: userData
     },
     (response) => location.reload())
-  }
+  },
 
+  setUserData() {
+    return {
+        critdayName: $('.dashboard__critday-name').val(),
+        critdayInstagram: $('.dashboard__critday-instagram').val(),
+        critdayCourse: $('.dashboard__critday-course').val(),
+        critdayProject: $('.dashboard__critday-project').val(),
+        critdaySite: $('.dashboard__critday-site').val(),
+        critdayUniversity: $('.dashboard__critday-university').val(),
+        critdayTutor: $('.dashboard__critday-tutor').val(),
+        critdayBio: $('.dashboard__critday-bio').val()
+    }
+  }
+  
 }
