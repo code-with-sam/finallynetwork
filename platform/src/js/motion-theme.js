b0189b1aa40b85b5e1b51a4c5580b6db047c0d6d
Sam Billingham <sambillie.sb@gmail.com>
2018-12-07 07:29:41 +0100

    motion theme - video layout & iframe player loader

--- platform/src/js/motion-theme.js
+++ platform/src/js/motion-theme.js
@@ -32,9 +32,13 @@ const motion = {
   blogFeedItemTemplate(post, featureImageSrc, tags, excerpt){
     const resteem = util.isResteem(motion.username, post) ? '' : `RESTEEM @${post.author} : `
     const link = util.getPostLink(motion.username, post)
+    const dtubePermlink = JSON.parse(post.json_metadata).video.info.permlink
 
     return `<div class="blog-feed__item">
-        <div class="blog-feed__item-feature" style="background-image: url(${featureImageSrc});">
+        <div class="blog-feed__item-feature"
+          style="background-image: url(${featureImageSrc});"
+          data-video="${dtubePermlink}"
+          data-username="${post.author}">
            <img class="item-feature__play" src="/img/motion-play.png">
         </div>
         <h2 class="blog-feed__item-title">${post.title}</h2> 
@@ -47,9 +51,36 @@ const motion = {
     <h2>${resteem}${post.title}</h2>
 
     ${html}`
+  },
+
+  additionalTemplate(){
+    return `<section>
+      <div class="overlay-bg"></div>
+      <div class="overlay">
+        <iframe id="video-frame" width="854" height="480" src="" allowfullscreen></iframe>
+      </div>
+    </section>`
+  },
+
+  themeActions() {
+    $('main').append(motion.additionalTemplate())
+
+    $('#video-frame').on('load', () => {
+      $('.overlay-bg').fadeIn()
+      setTimeout(()=> {
+        $('.overlay').fadeIn()
+      }, 400)
+    });
+
+    $('body').on('click', '.blog-feed__item-feature', (e) => {
+      const permlink = $(e.currentTarget).data('video')
+      const username = $(e.currentTarget).data('username')
+      $('#video-frame').attr('src', `https://emb.d.tube/#!/${username}/${permlink}/true`)
+    })
   }
 
 
 }
 
 motion.init()
+motion.themeActions()
